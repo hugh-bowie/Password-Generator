a009372cfebfc17a3f23cf4a2544d3f004585455
Hugh Bowie IV <hughbowie@me.com>
2021-08-06 13:13:54 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//set variables
+//set all variables
 let numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 let lowerCase = 'abcdefghijklmnopqrstuvwxyz'.split('');
 let upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
